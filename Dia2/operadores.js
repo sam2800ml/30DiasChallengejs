bc5fffb34dd4172c1c5b516310e689866a21d3fb
sam2800ml <sam2800ml@gmail.com>
2023-12-15 15:28:04 -0500

    se agrega el dia 2

---
+++ Dia2/operadores.js
@@ -0,0 +1,16 @@
+//Los operadores son elementos de lenguaje que permiten realizar calculos y comparaciones
+
+// Suma
+console.log(1 + 2); // 3
+
+// Resta
+console.log(5 - 2); // 3
+
+// Multiplicación
+console.log(3 * 4); // 12
+
+// División
+console.log(10 / 2); // 5
+
+// Módulo (devuelve el resto de una división)
+console.log(10 % 3); // 1
